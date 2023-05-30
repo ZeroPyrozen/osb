54fb6cc32f1be65d7dca6d6f2a56e9bf1ae162f8
EvT <44835662+Maks1mio@users.noreply.github.com>
2023-05-30 16:52:28 +0300

    + Modiefy FutureController.cs

--- Controllers/FutureController.cs
+++ Controllers/FutureController.cs
@@ -14,21 +14,88 @@ namespace osb.Controllers
     public class FutureController : BaseController
     {
         private IConfiguration _configuration;
+
         public FutureController(IConfiguration iconfig)
         {
             _configuration = iconfig;
         }
+
         public IActionResult Index(string code)
         {
-            if(!string.IsNullOrEmpty(code) && code == "WYSI")
+            if (!string.IsNullOrEmpty(code) && code == "WYSI")
                 return View();
             return Redirect("Home");
+        }
+
+        private BeatmapModel GetRandomBeatmap(List<BeatmapModel> beatmaps)
+        {
+            if (beatmaps != null & beatmaps.Count > 0)
+            {
+                List<BeatmapModel> beatmapHasVideo = beatmaps
+                    .Where(x => string.IsNullOrEmpty(x.VideoURL) == false)
+                    .ToList();
+                var random = new Random();
+                int index = random.Next(beatmapHasVideo.Count);
+                return beatmapHasVideo[index];
+            }
+            return null;
+        }
+
+        private int GetStoryboarderCount(List<BeatmapModel> beatmaps)
+        {
+            List<StoryboarderModel> countedStoryboarder = new List<StoryboarderModel>();
+            for (int i = 0; i < beatmaps.Count; i++)
+            {
+                for (int j = 0; j < beatmaps[i].Storyboarders.Count; j++)
+                {
+                    if (
+                        !countedStoryboarder.Any(
+                            x => x.UserID == beatmaps[i].Storyboarders[j].UserID
+                        )
+                    )
+                    {
+                        countedStoryboarder.Add(beatmaps[i].Storyboarders[j]);
+                    }
+                }
+            }
+            return countedStoryboarder.Count;
+        }
+
+        private List<KeyValuePair<string, int>> GetStoryboardMediumFrequency(
+            List<BeatmapModel> beatmaps
+        )
+        {
+            var distinctValues = beatmaps.GroupBy(p => p.Medium).Select(g => g.First()).ToList();
+            List<KeyValuePair<string, int>> mediumFrequency = new List<KeyValuePair<string, int>>();
+            for (int i = 0; i < distinctValues.Count; i++)
+            {
+                var count = beatmaps.Count(e => e.Medium == distinctValues[i].Medium);
+                mediumFrequency.Add(new KeyValuePair<string, int>(distinctValues[i].Medium, count));
+            }
+            return mediumFrequency.OrderByDescending(x => x.Value).ToList();
         }
 
         public IActionResult Home(string code)
         {
             if (!string.IsNullOrEmpty(code) && code == "WYSI")
-                return View();
+            {
+                HomeViewModel homeViewModel = new HomeViewModel();
+                homeViewModel.recentBeatmaps = DummyHelper.GenerateBeatmaps();
+                homeViewModel.totalStoryboard = homeViewModel.recentBeatmaps.Count;
+                homeViewModel.totalStoryboarder = GetStoryboarderCount(
+                    homeViewModel.recentBeatmaps
+                );
+                homeViewModel.mediumFrequency = GetStoryboardMediumFrequency(
+                    homeViewModel.recentBeatmaps
+                );
+                homeViewModel.randomBeatmap = GetRandomBeatmap(homeViewModel.recentBeatmaps);
+                homeViewModel.recentBeatmaps = homeViewModel.recentBeatmaps
+                    .OrderByDescending(x => x.ShowcasedDate)
+                    .Take(5)
+                    .ToList();
+                homeViewModel.baseURL = "https://" + this.Request.Host;
+                return View(homeViewModel);
+            }
             return Redirect("Home");
         }
 
