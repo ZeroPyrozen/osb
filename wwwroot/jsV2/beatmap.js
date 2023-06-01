846965749e2d9b0788a0a9e5aaae55f2c7490862
Maks1mio <one.evt@gmail.com>
2023-06-02 00:12:06 +0300

    Added play button style and script

---
+++ wwwroot/jsV2/beatmap.js
@@ -0,0 +1,23 @@
+let elements = document.querySelectorAll('.beatmap');
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= -200 &&
+        rect.left >= 0 &&
+        rect.bottom <= ((window.innerHeight + 200) || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+function onScroll() {
+    elements.forEach(element => {
+        if (isInViewport(element)) {
+            element.style.opacity = 1;
+            element.style.scale = 1;
+        } else {
+            element.style.opacity = 0;
+            element.style.scale = 0;
+        }
+    });
+}
+
+window.addEventListener('scroll', onScroll);
