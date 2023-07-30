d71ba3bdedc40553756fbbdda9c436d636d3d02f
EvT <one.evt@gmail.com>
2023-07-30 15:04:13 +0300

    Redesign icons, Finished community page, create resize for pages

--- wwwroot/jsV2/beatmap.js
+++ wwwroot/jsV2/beatmap.js
@@ -1,11 +1,15 @@
 let elements = document.querySelectorAll('.beatmap');
+
+if (elements.length === 0) {
+    elements = document.querySelectorAll('.user-card');
+}
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
         rect.top >= -200 &&
-        rect.left >= 0 &&
+        rect.left >= -200 &&
         rect.bottom <= ((window.innerHeight + 200) || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.right <= (window.innerWidth + 200 || document.documentElement.clientWidth)
     );
 }
 function onScroll() {
